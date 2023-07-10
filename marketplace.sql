7de398f583bbd246f7e19aef693dae2a0028d63e
QUENTOXX <74955277+QUENTOXX@users.noreply.github.com>
2023-07-10 15:42:57 +0200

    Add files via upload

---
+++ marketplace.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 10 juil. 2023 à 13:42
+-- Version du serveur : 5.7.40
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `marketplace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `Prénom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Mail` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Prix` int(11) DEFAULT NULL,
+  `Client` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Vendeur` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`Client`),
+  UNIQUE KEY `ID` (`ID`),
+  KEY `Vendeur` (`Vendeur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contrat`
+--
+
+DROP TABLE IF EXISTS `contrat`;
+CREATE TABLE IF NOT EXISTS `contrat` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livreur`
+--
+
+DROP TABLE IF EXISTS `livreur`;
+CREATE TABLE IF NOT EXISTS `livreur` (
+  `Prénom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Cmd_a_Livrer` int(11) NOT NULL,
+  KEY `Cmd_a_Livrer` (`Cmd_a_Livrer`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marketplace`
+--
+
+DROP TABLE IF EXISTS `marketplace`;
+CREATE TABLE IF NOT EXISTS `marketplace` (
+  `Nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Chiffre_d'affaire` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Prix` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vendeur`
+--
+
+DROP TABLE IF EXISTS `vendeur`;
+CREATE TABLE IF NOT EXISTS `vendeur` (
+  `Nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
